0130357174bc5444bc7b334aaeac085df9e43c69
SYM Developer <sym@sym.kz>
2026-06-18 10:24:44 +0500

    Recursion

---
+++ src/main/java/org/example/algo/study/queues/Queue.java
@@ -0,0 +1,27 @@
+package org.example.algo.study.queues;
+
+public class Queue {
+    private int maxSize;
+    private int[] arr;
+    private int front;
+    private int rear;
+    private int items;
+
+    public Queue(int maxSize) {
+        this.maxSize = maxSize;
+        arr = new int[maxSize];
+        front = 0;
+        rear = -1;
+        items = 0;
+    }
+
+    public boolean isEmpty() {
+        return front == rear;
+    }
+
+    public boolean isFull() {
+        return rear + 1 == maxSize;
+    }
+
+
+}
---
+++ src/main/java/org/example/algo/study/search/BinarySearchByUsingRecursion.java
@@ -0,0 +1,84 @@
+package org.example.algo.study.search;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+
+/**
+ * Complexity O(log n)
+ */
+public class BinarySearchByUsingRecursion {
+
+    private static final Logger log = LoggerFactory.getLogger(BinarySearchByUsingRecursion.class);
+
+    /**
+     *
+     * @param arr
+     * @param target
+     * @return index of element
+     */
+    public Optional<Integer> findInSortedArray(int[] arr, long target) {
+        if (arr == null || arr.length == 0) {
+
+            return Optional.empty();
+        }
+
+        log.info("Searching for: {}", target);
+        log.info("Array length: {}", arr.length);
+
+        int lower = 0;
+        int upper = arr.length - 1;
+        int idx;
+
+        log.info("lower: {}", lower);
+        log.info("upper: {}", upper);
+
+        while (true) {
+            idx = (lower + upper) / 2;
+
+            log.info("idx: {}", idx);
+
+            if (arr[idx] == target) {
+
+                return Optional.of(idx);
+
+            } else if (lower > upper) {
+
+                return Optional.empty();
+
+            } else {
+                if (arr[idx] < target) {
+                    lower = idx + 1;
+                } else {
+                    upper = idx - 1;
+                }
+
+                log.info("lower: {}", lower);
+                log.info("upper: {}", upper);
+            }
+        }
+    }
+
+    private Optional<Integer> findRec(int[] arr, int target, int lower, int upper) {
+        log.info("lower: {}", lower);
+        log.info("upper: {}", upper);
+
+        int idx = (lower + upper) / 2;
+        log.info("idx: {}", idx);
+
+        if (arr[idx] == target) {
+            return Optional.of(arr[idx]);
+        }
+
+        if (lower > upper) {
+            return Optional.empty();
+        }
+
+        if (arr[idx] < target) {
+            return findRec(arr, target, idx + 1, upper);
+        } else {
+            return findRec(arr, target, lower, idx - 1);
+        }
+    }
+}
