23964561b3a66f43c38a2a16b85c7c704d757004
SYM Developer <sym@sym.kz>
2026-06-24 10:18:22 +0500

    Binary Tree

--- src/main/java/org/example/algo/study/trees/Tree.java
+++ src/main/java/org/example/algo/study/trees/Tree.java
@@ -67,6 +67,7 @@ public class Tree {
             return;
         }
         inOrder(node.getLeftChild());
+        // return goes here when recursion chain starts
         node.displayNode();
         inOrder(node.getRightChild());
     }
