fc49f75524eb6d41d03f9d9e965e1742604e6219
SYM Developer <sym@sym.kz>
2026-07-28 10:05:01 +0500

    Longest Consecutive Subsequence

---
+++ src/main/java/org/example/algo/study/caterpillar/LongestConsecutiveSubsequence.java
@@ -0,0 +1,27 @@
+package org.example.algo.study.caterpillar;
+
+import java.util.Arrays;
+
+public class LongestConsecutiveSubsequence {
+
+    public int getLongestConsecutiveSubsequenceElementsCount(int[] a) {
+        if (a.length == 0) {
+            return 0;
+        }
+        Arrays.sort(a);
+        int count = 1;
+        int res = 1;
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] == a[i-1]) {
+                continue;
+            }
+            if (a[i] == a[i-1] + 1) {
+                count++;
+            } else {
+                count = 1;
+            }
+            res = Math.max(count, res);
+        }
+        return res;
+    }
+}
---
+++ src/test/java/org/example/algo/study/caterpillar/LongestConsecutiveSubsequenceTest.java
@@ -0,0 +1,18 @@
+package org.example.algo.study.caterpillar;
+
+import org.example.algo.study.ArrayTools;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongestConsecutiveSubsequenceTest {
+
+    @Test
+    void getLongestConsecutiveSubsequenceElementsCount() {
+        int [] a = {1, 2, 3, 5, 2, 2};
+        LongestConsecutiveSubsequence longestConsecutiveSubsequence = new LongestConsecutiveSubsequence();
+
+        assertThat(longestConsecutiveSubsequence.getLongestConsecutiveSubsequenceElementsCount(a)).isEqualTo(3);
+    }
+}
